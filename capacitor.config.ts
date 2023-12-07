02631715b11b637c7115e7ee5852f3e79fb7190c
Bhabani Sankar Padhy <102825973+bhabanisankar02@users.noreply.github.com>
2023-12-07 10:17:16 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'practice.project',
+  appName: 'practice-project',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
